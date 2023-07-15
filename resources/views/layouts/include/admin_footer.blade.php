923643d4ec89152233b6a73f22e41fcd468902c7
mayurlavadiya <mayurlavadiya123@gmail.com>
2023-07-15 14:36:57 +0530

    15-7-23

--- resources/views/layouts/include/admin_footer.blade.php
+++ resources/views/layouts/include/admin_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-4 bg-light">
     <div class="container-fluid px-4">
-        <div class="d-flex align-items-center justify-content-between small">
+        <div class="d-flex align-items-center justify-content-between medium">
             <div class="text-muted">Copyright &copy; TechBlog 2023</div>
             <div><b>Developed By :</b> Mayur Lavadiya</div>
             
--- resources/views/layouts/include/fronted-footer.blade.php
+++ resources/views/layouts/include/fronted-footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="py-4 bg-light">
+<footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
-        <div class="d-flex align-items-center justify-content-between small">
+        <div class="d-flex align-items-center justify-content-between medium">
             <div class="text-muted">Copyright &copy; TechBlog 2023</div>
             <div class="text-center">
                 <b>Developed By:</b> Mayur Lavadiya
